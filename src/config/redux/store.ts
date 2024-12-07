9338960fca311aaf252d248d0d7ca8e456e1046e
Sunday Toluwani Dere <62195195+dereSundayT@users.noreply.github.com>
2024-12-07 10:08:15 +0100

    feat: implement redux-persist to persist some data

--- src/config/redux/store.ts
+++ src/config/redux/store.ts
@@ -1,19 +1,52 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
+
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore,  FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER, } from 'redux-persist';
 
 import articleReducer from './articles/articleSlice'
 import appReducer from './app/appSlice'
 import userReducer from './user/userSlice'
 
 
-export const store = configureStore({
-    reducer: {
-        articles: articleReducer,
-        app: appReducer,
-        user: userReducer
-    },
+
+const rootPersistConfig = {
+    key: 'root',
+    storage:storage,
+    whitelist: ["user"]
+}
+
+
+const rootReducer = combineReducers({
+    articles: articleReducer,
+    app: appReducer,
+    user: userReducer
 })
+
+const persistedReducer = persistReducer(rootPersistConfig, rootReducer)
+
+
+export const store = configureStore({
+    reducer : persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+})
+
+
+
+
 
 
 export type RootState = ReturnType<typeof store.getState>
 
 export type AppDispatch = typeof store.dispatch
+
+export const persist = persistStore(store)
